aba23aa21a28ef5aed878d2094bbd0acd03f74b2
kdelange <kimatwork01@gmail.com>
2024-01-25 10:49:59 +0100

    circleci and check folder added

---
+++ check/indentationcheck.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MYDIR="$(cd -P "$(dirname "${0}")" && pwd)"
+
+echo '#####################################################################################'
+echo '  Bash code must be indented with TABs. Checking for lines indented with spaces ... '
+echo '#####################################################################################'
+grep --recursive --line-number --include='*.*sh' '^[[:space:]]* [[:space:]]*' "${MYDIR}"/../
+grep_exit_status="${?}"
+if [[ "${grep_exit_status}" -eq 0 ]]; then
+	echo '#####################################################################################'
+	echo '  ERROR: found Bash files containing lines (partially) indented with spaces.'
+	echo '#####################################################################################'
+	exit 1
+elif [[ "${grep_exit_status}" -eq 1 ]]; then
+	echo '  Ok: all Bash files indented with TABs.'
+	echo '####################################################################################'
+	exit 0
+else
+	echo '  FAILED: indentation sanity check failed.'
+	echo '####################################################################################'
+	exit 1
+fi

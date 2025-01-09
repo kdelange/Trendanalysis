44e7a4cf498b0efefeb992ffedda95dcf9b05d62
kdelange <kimatwork01@gmail.com>
2025-01-09 13:11:41 +0100

    testing

--- bin/copyQCDataToTmp.sh
+++ bin/copyQCDataToTmp.sh
@@ -98,7 +98,7 @@ function copyQCProjectdataToTmp() {
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    from ${_prm_project_dir}"
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    to ${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/"
 		echo "${_line_base}.failed" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 			return
 			}
 		rsync -av --rsync-path="sudo -u ${group}-ateambot rsync" "${_prm_project_dir}/${_project}/run01/results/${_project}.csv" "${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/trendanalysis/RNAprojects/${_project}/" \
@@ -107,13 +107,13 @@ function copyQCProjectdataToTmp() {
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    from ${_prm_project_dir}"
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    to ${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/"
 		echo "${_line_base}.failed" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 			return
 			}
 		sed "/${_line_base}.failed/d" "${_project_job_controle_file_base}" > "${_project_job_controle_file_base}.tmp"
 		sed "/${_line_base}.started/d" "${_project_job_controle_file_base}" > "${_project_job_controle_file_base}.tmp"
 		echo "${_line_base}.finished" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 		log4Bash 'TRACE' "${LINENO}" "${FUNCNAME:-main}" '0' "Finished copying project: ${_project}"
 	# The inhouse projects (Exoom, targeted) will be copied to ${TMP_ROOT_DIR}/trendanalysis/projects/
 	elif [[ -e "${_prm_project_dir}/${_project}/run01/results/multiqc_data/${_project}.run_date_info.csv" ]]
@@ -125,7 +125,7 @@ function copyQCProjectdataToTmp() {
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    from ${_prm_project_dir}/"
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    to ${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/"
 		echo "${_line_base}.failed" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 		return
 		}
 		rsync -av --rsync-path="sudo -u ${group}-ateambot rsync" "${_prm_project_dir}/${_project}/run01/results/${_project}.csv" "${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/trendanalysis/projects/${_project}/" \
@@ -134,13 +134,13 @@ function copyQCProjectdataToTmp() {
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    from ${_prm_project_dir}/"
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    to ${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/"
 		echo "${_line_base}.failed" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 			return
 			}
 		sed "/${_line_base}.failed/d" "${_project_job_controle_file_base}" > "${_project_job_controle_file_base}.tmp"
 		sed "/${_line_base}.started/d" "${_project_job_controle_file_base}" > "${_project_job_controle_file_base}.tmp"
 		echo "${_line_base}.finished" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 		log4Bash 'TRACE' "${LINENO}" "${FUNCNAME:-main}" '0' "Finished copying project: ${_project}"
 	# The Dragen project (Exoom, WGS, sWGS) wil be copied to ${TMP_ROOT_DIR}/trendanalysis/dragen/
 	elif  [[ -e "${_prm_project_dir}/${_project}/run01/results/qc/statistics/${_project}.Dragen_runinfo.csv" ]]
@@ -153,7 +153,7 @@ function copyQCProjectdataToTmp() {
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    from ${_prm_project_dir}/"
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    to ${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/"
 		echo "${_line_base}.failed" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 			return
 			}
 		rsync -av --rsync-path="sudo -u ${group}-ateambot rsync" "${_prm_project_dir}/${_project}/run01/results/${_project}.csv" "${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/trendanalysis/dragen/${_project}/" \
@@ -162,13 +162,13 @@ function copyQCProjectdataToTmp() {
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    from ${_prm_project_dir}/"
 		log4Bash 'ERROR' "${LINENO}" "${FUNCNAME:-main}" '0' "    to ${DESTINATION_DIAGNOSTICS_CLUSTER}:${TMP_ROOT_DIR}/"
 		echo "${_line_base}.failed" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 		return
 		}
 		sed "/${_line_base}.failed/d" "${_project_job_controle_file_base}" > "${_project_job_controle_file_base}.tmp"
 		sed "/${_line_base}.started/d" "${_project_job_controle_file_base}" > "${_project_job_controle_file_base}.tmp"
 		echo "${_line_base}.finished" >> "${_project_job_controle_file_base}.tmp"
-		mv "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
+		cp "${_project_job_controle_file_base}.tmp" "${_project_job_controle_file_base}"
 		log4Bash 'TRACE' "${LINENO}" "${FUNCNAME:-main}" '0' "Finished copying project: ${_project}"
 	else
 		log4Bash 'TRACE' "${LINENO}" "${FUNCNAME[0]:-main}" '0' "For project ${_project} there is no QC data, nothing to rsync.."
